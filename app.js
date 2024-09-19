dc4608f447fb32dc53c34d707924958f347b8126
andresog14 <53319410+andresog14@users.noreply.github.com>
2024-09-19 18:28:47 -0500

    Update app.js
    ajuste de ruta raiz

--- app.js
+++ app.js
@@ -3,14 +3,23 @@ var bodyParser = require('body-parser');
 var app = express();
 var port = process.env.PORT || 8080;
 
+// Middleware para manejar JSON y datos codificados
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Importar las rutas desde la carpeta routes
 var router = require('./routes');
 app.use('/api', router);
 
+// Definir la ruta raíz
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Bienvenido a la API de prueba' });
+});
+
+// Iniciar el servidor
 app.listen(port, () => {
   console.log(`API escuchando en el puerto ${port}`);
 });
 
 module.exports = app;
+
