2f9c994f1f54f91597fa49c8160d05286d5b3a2a
andresog14 <53319410+andresog14@users.noreply.github.com>
2024-09-19 18:50:10 -0500

    Update index.js
    cambio en index

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
-var router = require('express').Router();
-var cervezasRoutes = require('./cervezas');
+const express = require('express');
+const router = express.Router();
+const cervezasRoutes = require('./cervezas');
 
+// Ruta para cervezas
 router.use('/cervezas', cervezasRoutes);
 
+// Ruta para el prefijo /api
 router.get('/', (req, res) => {
   res.status(200).json({ message: 'Conectado a la API' });
 });
 
 module.exports = router;
+
